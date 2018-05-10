ef2d332ccc776d688fe9b7ffde7c334380afac43
Janice Lu <jjlu555@gmail.com>
2018-05-09 19:11:33 -0700

    Add files via upload

--- htdocs/buy_book.php
+++ htdocs/buy_book.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 require 'item.php';
+require 'account.php';
 $conn = new mysqli("localhost","root","root","Textbook_Exchange");
 
 $name = $_SESSION['Username'];
-
-echo $name;
 
 if($conn->connect_error)
 {
@@ -14,21 +13,44 @@ if($conn->connect_error)
 if(isset($_GET['book']))
 {
 	$booksid = $_GET['book'];
+	
+	$query0 = "SELECT * from Book where BookID = '";
+	$query0 .= $booksid;
+	$query0 .= "'";
+	$result = mysqli_query($conn, $query0);
+	$product = mysqli_fetch_object($result);
+	$item = new Item();
+	$item->Name = $product->Name;
+
 	$query2 = "DELETE FROM Book WHERE BookID = '";
 	$query2 .= $booksid;
 	$query2 .= "'";
-	$result1 = mysqli_query($conn, $query2);
-	$product = mysqli_fetch_object($result1);
 
 }
 
-if($conn->query($query2) === TRUE)
+
+if(isset($_GET['id']))
 {
-    header('Location: cart.php?index=0');
+	$seller = $_GET['id'];
+	$query1 = "INSERT into Transactions (Name, SellerID, BuyerID) values ('$item->Name', '$seller', '$name')";
+}
+
+
+if($conn->query($query1) == TRUE)
+{
+	if($conn->query($query2) === TRUE)
+	{
+		header('Location: cart.php?index=0');
+	}
+	else
+	{
+		echo "Error: ". $query2. "<br>" . $conn->error;
+	}
 }
 else
 {
-    echo "Error: ". $query2. "<br>" . $conn->error;
+		echo "Error: ". $query1. "<br>" . $conn->error;
 }
+
 
 ?>
